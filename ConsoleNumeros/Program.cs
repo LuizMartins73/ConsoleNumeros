04a20e52c4f0ab3e6241091cebbd5806ea033eb2
Usuario <Usuario@CPD02-M07>
2024-05-07 12:08:49 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleNumeros/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a quantidade de números a serem escritos: ");
+            int qtd = int.Parse(Console.ReadLine());
+            double[] numero = new double[qtd];
+            double maiornumero = double.MinValue;
+            double menornumero = double.MaxValue;
+            for (int j = 0; j<qtd; j++)
+            {
+                Console.WriteLine("Digite o número: ");
+                numero[j] = double.Parse(Console.ReadLine());
+             
+               
+            }
+            Console.Write("Números digitados: ");
+            for (int j=0; j<qtd; j++)
+            {
+                Console.Write(numero[j] + "    ");
+                if (numero[j] <= menornumero) { menornumero=numero[j];}
+                if (numero[j]>=maiornumero) { maiornumero=numero[j]; }
+            }
+        
+            Console.WriteLine("\nMaior número: "+maiornumero);
+            Console.WriteLine("Menor número: "+menornumero); 
+         
+              
+            Console.ReadKey();
+
+        }
+    }
+}
